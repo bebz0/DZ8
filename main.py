07bbe727be8e21f900228551c29c6174c9d6d1e8
bebz0 <romanbizonua@gmail.com>
2025-03-25 10:02:52 +0200

    made class Hand

--- main.py
+++ main.py
@@ -31,3 +31,23 @@ class Digit:
                 x = self.radius * 0.85 * math.cos(angle * 3.14159 / 180)
                 y = self.radius * 0.85 * math.sin(angle * 3.14159 / 180)
                 Digit(x, y, i).draw()
+
+
+class Hand:
+
+    def __init__(self, length, width, color):
+        self.length = length
+        self.width = width
+        self.color = color
+
+    def draw(self, angle):
+        turtle.penup()
+        turtle.goto(0, 0)
+        turtle.setheading(angle)
+        turtle.pendown()
+        turtle.pensize(self.width)
+        turtle.pencolor(self.color)
+        turtle.forward(self.length)
+        turtle.penup()
+        turtle.goto(0, 0)
+        turtle.pendown()
